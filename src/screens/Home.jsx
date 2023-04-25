59e9a03daeb39ba0ebdfd6ac757ec8a88b25368a
Luc <lucescomel@gmail.com>
2023-04-25 14:15:35 +0200

    modif button

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -65,7 +65,7 @@ export default function Home() {
             onChange={handleInputChange}
           />
         </div>
-        <button type="submit">connect</button>
+        <button className="login-button" type="submit">connect</button>
       </form>
     </div>
   );
